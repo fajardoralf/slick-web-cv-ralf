43566c1f47bf5454879b67117e13a74b10ca5b3f
fajardoralf <fajardoralf@gmail.com>
2019-03-14 15:05:17 +0100

    first commit

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODO LIST
+
+1. Design the JSON file
+2. Read JSON file - store in variables
+3. Webpage architecture
+    - '/' = index.html + index.js
+    - '/data' = show json
+    - others?
+        - https://www.themuse.com/advice/the-35-best-personal-websites-weve-ever-seen
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const pug = require('pug');
+const port = 8081
+
+// load data from data.json
+let rawData = fs.readFileSync("person.json");
+const data = JSON.parse(rawData);
+
+// Set static resources
+app.use(express.static(path.join(__dirname + '/resources')));
+
+
+// pug
+app.set('view engine', 'pug')
+
+// routes
+app.get('/data', (req, res) => res.send(data));
+
+app.get('/', (req, res) => {
+   res.render('index', data)
+});
+
+app.listen(process.env.PORT || port)
